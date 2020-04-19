52c764b1bab4bdbefe79d8e6fd9068ed3063ba79
Dima Nazarchuk <dmitry.nazarchuk.work@gmail.com>
2020-04-19 14:05:50 +0300

    Add created_at attr to post serialization

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :body, :title, :status
+  attributes :id, :body, :title, :status, :created_at
+
+  def created_at
+    object.created_at.strftime('%B %d, %Y')
+  end
 end
